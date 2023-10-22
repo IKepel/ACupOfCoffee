2a0a697a8f6c63e3e2b21eada370db9c303b536d
Kostya Popov <kepel2823@gmail.com>
2023-10-22 13:53:23 +0300

    Add project files.

---
+++ ACupOfCoffee/Entities/Cup.cs
@@ -0,0 +1,37 @@
+﻿using ACupOfCoffee.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACupOfCoffee.Entities
+{
+    public class Cup : CupBase, IContainingCoffee, IContainingWater, IContainingMilk, IContainingSugar
+    {
+        public void AddCoffee()
+        {
+            Console.WriteLine("Add coffee to the cup");
+        }
+
+        public void AddMilk()
+        {
+            Console.WriteLine("Add milk to the cup");
+        }
+
+        public void AddSugar()
+        {
+            Console.WriteLine("Add sugar to the cup");
+        }
+
+        public void AddWater()
+        {
+            Console.WriteLine("Add water to the cup");
+        }
+
+        public override void Stir()
+        {
+            Console.WriteLine("Stirring...");
+        }
+    }
+}
---
+++ ACupOfCoffee/Program.cs
@@ -0,0 +1,33 @@
+﻿using ACupOfCoffee.Contracts;
+using ACupOfCoffee.Entities;
+
+var cup = new Cup();
+
+AddCoffee(cup);
+AddWater(cup);
+cup.Stir();
+AddMilk(cup);
+cup.Stir();
+AddSugar(cup);
+cup.Stir();
+Console.WriteLine("Your coffee's ready.");
+
+void AddCoffee(IContainingCoffee cup)
+{
+    cup.AddCoffee();
+}
+
+void AddMilk(IContainingMilk cup)
+{
+    cup.AddMilk();
+}
+
+void AddSugar(IContainingSugar cup)
+{
+    cup.AddSugar();
+}
+
+void AddWater(IContainingWater cup)
+{
+    cup.AddWater();
+}
